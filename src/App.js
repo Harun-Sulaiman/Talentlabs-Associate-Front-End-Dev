f29bace9fac0239683166f5390f41cf9dd60fa4a
Azrun_SS <harunsantuso@gmail.com>
2023-01-09 15:31:36 +0800

    A4 Submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Clock from './components/Clock';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [flag, setFlag] = useState(true);
+
+
+const toggle = () => { 
+  console.log ("Toggle Clicked");
+  setFlag (!flag); 
+  };
+  return ( 
+    <>
+      <button onClick= {toggle}>Toggle Clock Component</button>
+      {flag ? <Clock /> : "Clock Off"}
+  
+    </>
   );
 }
 
