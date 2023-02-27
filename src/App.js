234d72da8219c64adbae4219d064e12e866af898
Azrun_SS <harunsantuso@gmail.com>
2023-02-27 17:11:23 +0800

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
-// import { useState } from 'react';
 import './App.css';
-import ClockControl from './components/ClockControl';
-
+import Form from './components/Form';
 function App() {
 
   return ( 
     <>
-      <ClockControl />
+      <Form />
     </>
   );
 }
