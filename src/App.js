b53d48e23ba3df3c70ae9052beca707e82888728
Azrun_SS <harunsantuso@gmail.com>
2023-02-28 16:11:48 +0800

    modified:   src/App.js 	deleted:    src/components/Form.js 	new file:   src/components/List.js 	new file:   src/components/Search.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import { useState } from 'react';
 import './App.css';
-import Form from './components/Form';
+import List from './components/List';
+import Search from './components/Search';
 function App() {
+const [searchTerm, setSearchTerm] = useState ();
+const handleSearch = (e) => {
+  setSearchTerm(e.target.value);
+};
+
+const posts = [
+  {
+    id: 1,
+    title: "Item 1",
+    content: "Item 1 description",
+  },
+  {
+    id: 2,
+    title: "keyboard",
+    content: "keyboard description",
+  },
+  {
+    id: 3,
+    title: "mouse",
+    content: "mouse description",
+  },
+  {
+    id: 4,
+    title: "desktop",
+    content: "desktop description",
+  },
+  {
+    id: 5,
+    title: "power supply",
+    content: "power supply description",
+  },
+]
+
+const filterList = posts.filter ((item) =>{
+  return item.title.toLocaleLowerCase().includes(searchTerm);
+})
 
   return ( 
     <>
-      <Form />
+      <Search searchTerm = {searchTerm} handleSearch={handleSearch}/>
+      <List  list= {filterList}/>
     </>
   );
 }
